370e3bf6135b65d87db53c920c3bc4ce3d5d1696
5kipp3r <112339635+Th3m4rk5m4n@users.noreply.github.com>
2022-11-23 10:05:24 +0530

    Add files via upload

---
+++ nested if(number is zero , positive , negative ).py
@@ -0,0 +1,9 @@
+# nestad if 
+num = float(input("enter a number: "))
+if num >= 0:
+    if num == 0:
+        print("zero")
+    else:
+        print("positive number")
+else:
+    print("negative number")
